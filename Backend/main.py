437c011c03c96dfbbe5e183a2e40fb4eca2f566c
Moises <moisvara97@gmail.com>
2026-02-12 21:17:31 -0500

    Backend Update

---
+++ Backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app= FastAPI()
+
+# CORS: Cross Origin Resource Sharing , Se utiliza para habilitar peticiones desde clientes que no estan en mi dominio, tiene que hacerse de manera explicita porque se deben considerar cuales son las reglas para aceptar esos números. 
+#CORSMiddleware → Lo que se interpone entre mi programa y el medio 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    all_credentials= True, 
+    allow_methods=["*"],
+)
+
+
+@app.get("./sumar")
+def sumar_numeros(a:float, b:float):
+    return a+b
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Para el Backend
 
-1. Crear un ambiente virtual
-2. Instalar la biblioteca FastAPI (pip install fast api)
+1. Crear un ambiente virtual --> Python -m venv .myenv , luego le hacemos .\.myenv\Scripts\activate
+2. Instalar la biblioteca FastAPI --> (pip install fast api)
